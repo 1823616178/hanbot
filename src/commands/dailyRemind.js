7f91ce55edef7e88c73603e5de15e0e49ca95169
lx1823616178 <lx1823616178@outlook.com>
2021-12-10 18:01:44 +0800

    check

--- src/commands/dailyRemind.js
+++ src/commands/dailyRemind.js
@@ -4,14 +4,15 @@ const {onebot} = require('../utils/bots')
 module.exports = () => {
     koishi.command("alarmClock")
         .action(({session}) => {
-            let botList = koishi.bots[0].getGroupMemberMap("792469853").then(res => {
+            koishi.bots[0].sendPrivateMessage('2787200983', "猪猪猪猪猪猪猪起床")
+            /*let botList = koishi.bots[0].getGroupMemberMap("792469853").then(res => {
                 for (let i in res) {
                     if (i !== '3185128029') {
-                        session.send(segment.at(i) + '猪猪猪猪猪猪猪起床')
-                        koishi.bots[0].sendMessage('792469853', "12312312312")
+                        // session.send(segment.at(i) + '猪猪猪猪猪猪猪起床')
+                        koishi.bots[0].sendPrivateMessage('2787200983', "猪猪猪猪猪猪猪起床")
                     }
                 }
-            })
+            })*/
             // session.send("sey hello")
         })
 
